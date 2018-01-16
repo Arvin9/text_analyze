b88d01a5a48f045ac41370c371fa4b60d981b5ee
Arvin9 <594113869@qq.com>
2018-01-17 00:23:18 +0800

    init

---
+++ app/controller/home.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+const Logger = require('egg-logger').Logger;
+const ConsoleTransport = require('egg-logger').ConsoleTransport;
+const logger = new Logger();
+logger.set('console', new ConsoleTransport({
+    level: 'DEBUG',
+}));
+
+class HomeController extends Controller {
+  async index() {
+    logger.info("index");
+    let obj = new Object();
+    obj.name = "hello";
+    await this.ctx.render('hello.ejs', {
+        data: 'world',
+    });
+  }
+}
+
+module.exports = HomeController;
---
+++ config/config.prod.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = appInfo => {
+    const config = {};
+
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1515592873430_3158';
+
+    // add your config here
+    config.middleware = [];
+
+    /**
+     * Static file serve
+     *
+     * @member Config#static
+     * @property {String} prefix - `/public/` by default
+     * @property {String} dir - static files store dir, `${baseDir}/app/public` by default
+     * @property {Number} maxAge - cache max age, default is 0
+     * @see https://github.com/koajs/static-cache
+     */
+    config.static = {
+        prefix: '/public/',
+        dir: path.join(appInfo.baseDir, 'app/public'),
+        // support lazy load
+        dynamic: true,
+        preload: false,
+        buffer: false,
+        maxFiles: 1000,
+    };
+    return config;
+};
